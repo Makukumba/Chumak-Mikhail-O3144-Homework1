1a9df4bbb491ef60c3fe9982a7f022d725f95648
Makukumba <100723151+Makukumba@users.noreply.github.com>
2022-03-02 23:13:57 +0300

    Add files via upload

---
+++ Ex2_1Ez.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Ex2_1Ez{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+        String a = "hello";
+        if (text.equals(a))
+        {System.out.println(a + " world!");
+    }
+}
+}
